6b258b59cbbcc9e2ee7378a08e11673eaa8c111e
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2021-02-19 17:56:31 -0300

    teste

--- src/main/java/com/sm/SoftmotorsApiApplication.java
+++ src/main/java/com/sm/SoftmotorsApiApplication.java
@@ -1,13 +1,26 @@
 package com.sm;
 
+//import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class SoftmotorsApiApplication {
+public class SoftmotorsApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoftmotorsApiApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+		return builder.sources(SoftmotorsApiApplication.class);
 	}
 
 }
